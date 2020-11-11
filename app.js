ecea1a0ffd8cbdf19b043e36182450e961be9136
OlgaGarbar <mamapsih87@gmail.com>
2020-11-11 21:32:35 +0200

    11.11.2020

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log("Hello world!");
+let x = 4;
+console.log(x);
+x = x + 2;
+console.log(x);
+/*
+instructiuni de decizie
+*/ 
+
+let nota = 8;
+if(nota === 10){
+    console.log("Bravo, URA!!!");
+}else if( nota < 10 && nota >= 5){
+    console.log("Inveti normal");
+}
+else{
+    console.log("Inveti rau(((");
+}
+
+
+console.log(0);
+console.log(1);
+console.log(2);
+console.log(3);
+
+for (let cifra = 0; cifra < 10; cifra++) {
+    console. log(cifra);
+    
+}
+console.log("WHILE");
+let cifra = 0;
+while ( cifra < 10) {
+    console. log(cifra);
+    cifra++;
+}
+console.log("END");
+
+console.log(" do WHILE");
+ cifra = 0;
+while ( cifra < 10) {
+    console. log(cifra);
+    cifra++;
+    do{
+        console log(cifra);
+        cifra++;
+    }
+    while(cifra , 10);
+    console.log("end");
+
+    console.log("Numere de la 100 pina la 0");
+    for (let numar = 100; numar >= 0; numar--){;
+     console.log(numar);   
+    }
+    
---
+++ app1.js
@@ -0,0 +1,45 @@
+let numere=[2,5]
+console.log(numere);
+
+numere.push(8);
+numere.unshift(3);
+console.log(numere);
+
+for(let index = 0; index<numere.length; index++){
+    console.log(numere[index]);
+}
+let suma = 0;
+numere.forEach(numar=>{
+    suma = suma + numar;
+
+});
+console.log(suma);
+console.clear();
+
+const luni = [
+    "Ianuarie", "Februarie", "Martie","aprilie", "Mai",
+ "Iunie", "Iulie", "August","Septembrie", "Octombrie",
+ "Noiembrie","Decembrie"
+];
+console.log(luni);
+
+for(const key in luni) {
+    if(luni.hasOwnProperty(key)){
+        console.log(luni[key]);
+    }
+}
+console.log("**********");
+const numarulLunii = 8;
+console.log(luni[numarulLunii - 1]);
+
+console.clear();
+
+let numbers = [];
+for (let index = 0; index <= 10; index++) {
+
+    numbers.push(index);
+}
+console.log(numbers);
+console.log(numbers.filter(element => element % 2 === 0));
+console.log(numbers.map(element=> element ** 2));
+console.log(numbers.find(element=> element === 8 ));
